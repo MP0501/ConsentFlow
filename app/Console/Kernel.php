64158eb1b9803e61154b453aa844c790ec7f6307
MP0501 <moritz.pukrop@gmail.com>
2024-03-25 19:57:32 +0100

    remove send invoice

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,19 +2,11 @@
 
 namespace App\Console;
 
-use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [
-        \App\Console\Commands\SendInvoices::class,
-    ];
-
-    protected function schedule(Schedule $schedule): void
-    {
-        $schedule->command('invoices:send')->monthlyOn(1, '00:00');
-    }
+    
 
 
 
