42f22a6346ba1021362583653ba3363dcd51b143
fynn-grab <fynngrab@gmail.com>
2024-03-25 19:56:28 +0100

    Kernel Änderungen

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,10 +7,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    protected $commands = [
-        \App\Console\Commands\SendInvoices::class,
-    ];
-
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('invoices:send')->monthlyOn(1, '00:00');
